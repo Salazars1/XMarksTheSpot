90f04743883eff2bba6063d881ffc7b793491dea
nelsona11 <nelsona11@xavier.edu>
2018-03-13 19:55:46 -0400

    stuff

--- buildings/models.py
+++ buildings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Building(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Floor(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    name = models.IntegerField(default=0)
+
+
+class Room(models.Model):
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
+    building = floor.building
+    name = models.IntegerField(default=0)
