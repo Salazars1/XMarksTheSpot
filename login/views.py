f2bac4182f9eb093346b90754a8d129ced19f529
nelsona11 <nelsona11@xavier.edu>
2018-03-18 15:34:30 -0400

    can create accounts now
    the html has ben changed sigificantly at this point. we can change it back, but i need to figure out how to do the authentication with our own code

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -7,5 +8,16 @@ def index(request):
     return render(request, 'registration/login.html')
 
 def createAccount(request):
-    return render(request, 'Website/CreateAccount.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/home/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Website/CreateAccount.html', {'form': form})
 
