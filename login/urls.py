f85d524f4924ce1cfedbff0600e7009808132d47
nelsona11 <nelsona11@xavier.edu>
2018-03-18 13:37:40 -0400

    slight changes to login

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    url('', auth_views.login, name='login'),
+    path('', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^admin/', admin.site.urls),
+    url('/admin/', admin.site.urls),
+    path('create-account/', views.createAccount, name='createAccount'),
 ]
