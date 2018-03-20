1d8eff0db22b1419c3edd564e2d2fdc6b6382888
nelsona11 <nelsona11@xavier.edu>
2018-03-19 20:10:43 -0400

    changed how rooms and floors are viewed

--- buildings/models.py
+++ buildings/models.py
@@ -12,8 +12,18 @@ class Building(models.Model):
 class Floor(models.Model):
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
     name = models.IntegerField(default=0)
+    def __str__(self):
+        buildingName = self.building.name
+        return buildingName + " " + str(self.name)
 
 
 class Room(models.Model):
     floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
     name = models.IntegerField(default=0)
+    def __str__(self):
+        buildingName = self.floor.building.name
+        floorName = str(self.floor.name)
+        roomName = floorName + str(self.name)
+        if self.name < 10:
+            roomName = floorName + str(0) + str(self.name)
+        return buildingName + " " + roomName
