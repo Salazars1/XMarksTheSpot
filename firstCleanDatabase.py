bfe87b164815cda21372b3572ea1d00c42670abc
YasminAR16 <36425880+YasminAR16@users.noreply.github.com>
2018-03-19 20:19:52 -0400

    Add files via upload
    added a python script to start with clean database

---
+++ firstCleanDatabase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import MySQLdb
+
+#connect to database
+db=MySQLdb.connect(host="localhost",user="root", passwd="2016x") 
+# Create a Cursor object to execute queries.
+cursor = db.cursor()
+# delete database using SQL query.
+cursor.execute("DROP DATABASE IF EXISTS DBXMarksTheSpot")
+# Commit your changes in the database
+db.commit()
+# create database using SQL query.
+f = open('DBcode.sql', 'r')
+query = " ".join(f.readlines())
+cursor.execute(query)
+# disconnect from server
+db.close()
