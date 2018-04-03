61820c52d8e640d94e98b5e6372c661d232d8a7f
Yasmin Alvarado-Rayo <yasminrayo@outlook.com>
2018-04-03 19:58:43 -0400

    adding test cases for view profile

--- viewProfile/tests.py
+++ viewProfile/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from django.test import Client
+from django.contrib.auth.models import User
 # Create your tests here.
+class ViewProfilePageTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+    def test_ViewProfilePage(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'testuser', 'password': 'secret'})
+        response = c.get('/profile/')
+        self.assertEqual(response.status_code, 200)
