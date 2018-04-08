bd98949271f22fb4474ea0d26fef923f0eccc9b4
nelsona11 <nelsona11@xavier.edu>
2018-04-08 19:10:34 -0400

    sort of limits to xavier students

--- login/views.py
+++ login/views.py
@@ -9,17 +9,21 @@ def index(request):
     return render(request, 'registration/login.html')
 
 def createAccount(request):
+    response = ''
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            request.session['name'] = username
-            return redirect('/home/')
+            if '@xavier.edu' in form.cleaned_data.get('email'):
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                request.session['name'] = username
+                return redirect('/home/')
+            else:
+                response = 'Please enter valid Xavier email.'
     else:
         form = CreateUserForm()
-    return render(request, 'Website/CreateAccount.html', {'form': form})
+    return render(request, 'Website/CreateAccount.html', {'form': form, 'response':response})
 
