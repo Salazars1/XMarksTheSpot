247a18cca0b8365c811d8d29537f6ccd75ffb37b
Santiago Salazar <salazars1@xavier.edu>
2018-04-17 17:05:09 -0400

    changes in settings.py

--- XMarksTheSpot/settings.py
+++ XMarksTheSpot/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'DBXMarksTheSpot',
-        'USER': 'xmarksthespot',
+        'USER': 'root',
         'PASSWORD': 'xmarksthespot',
     }
 }
